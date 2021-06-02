c22aa5f208fd67561ceefc96e8649f924dd097e5
Patrick <tunezepatrick@gmail.com>
2021-06-02 13:49:11 +0200

    login functionality

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useEffect, useState } from "react";
 import Login from "./components/Login";
+import { getToken } from "./config/spotify";
 
 function App() {
-  return (
-    <div className="app">
-      <Login />
-    </div>
-  );
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getToken();
+    window.location.hash = ""; // REMOVE THE TOKEN FROM URL AFTER GRABBING IT, FOR SECURITY
+    const _token = hash.access_token;
+    if (_token) setToken(_token);
+  }, [token]);
+
+  return <div className="app">{!token ? <Login /> : <h1>Logged</h1>}</div>;
 }
 
 export default App;
